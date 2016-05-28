115804bfacaf2cf9f90d99f868a10cedb6938e3b
Brendan Rius <brendan@omixy.com>
2016-05-28 13:12:29 +0100

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='simplefsabstraction',
+      version='1.0.0',
+      description='A (very) simple FS/S3 abstraction',
+      author='Brendan Rius',
+      author_email='ping@brendan-rius.com',
+      url='https://github.com/brendanrius/simplefsabstraction/',
+      download_url='https://github.com/brendanrius/simplefsabstraction/tarball/1.0.0',
+      packages=['simplefsabstraction'],
+      keywords=['fs', 's3'])
